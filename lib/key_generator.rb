742aca3265c71058af23f145b994f0c4fc341124
brickstar <brickstar@gmail.com>
2018-02-05 16:46:43 -0700

    working on file structure for key generator

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,15 +1,22 @@
 class KeyGenerator
 
+  def initialize
+
+  end
+  
+  def random_key
+    @key = rand(9999..100000)
+  end
+
   def find_key
-    @key_array = []
-    random_a = @key[0..1].to_i      
+    random_a = @key[0..1].to_i
     random_b = @key[1..2].to_i
     random_c = @key[2..3].to_i
     random_d = @key[3..4].to_i
-    date_a   = @date_today[0].to_i
-    date_b   = @date_today[1].to_i
-    date_c   = @date_today[2].to_i
-    date_d   = @date_today[3].to_i
+  end
+
+  def offset_calculator
+    @key_array = []
     @key_array << random_a + date_a
     @key_array << random_b + date_b
     @key_array << random_c + date_c
---
+++ lib/offset_calculator.rb
@@ -0,0 +1,18 @@
+class OffsetCalculator
+
+  def offset_calculator
+    @key_array << random_a + date_a
+    @key_array << random_b + date_b
+    @key_array << random_c + date_c
+    @key_array << random_d + date_d
+  end
+
+  def offset
+    date_a   = @date_today[0].to_i
+    date_b   = @date_today[1].to_i
+    date_c   = @date_today[2].to_i
+    date_d   = @date_today[3].to_i
+  end
+
+
+end
