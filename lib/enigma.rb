f684d369b3754945cc6346fe079328ddc188b04a
brickstar <brickstar@gmail.com>
2018-02-06 14:37:06 -0700

    restructuring ----- again...

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,9 +3,36 @@ require './lib/encrypt.rb'
 require './lib/decrypt.rb'
 
 class Enigma
+  include Cipher
+  
   attr_reader :message
 
-  def initialize(message)
+  def initialize(message, key, date)
     @message = message
+    @key     = key
+    @date    = date
   end
+
+  def encrypt(message)
+    message = message.map do |array|
+    result = []
+    result << cipher(63)[array[0]]
+    result << cipher(72)[array[1]]
+    result << cipher(73)[array[2]]
+    result << cipher(14)[array[3]]
+    end
+    message.join
+  end
+
+  def decrypt(message)
+    message = message.map do |array|
+    result = []
+    result << cipher(-63)[array[0]]
+    result << cipher(-72)[array[1]]
+    result << cipher(-73)[array[2]]
+    result << cipher(-14)[array[3]]
+    end
+    message.join
+   end
+
 end
